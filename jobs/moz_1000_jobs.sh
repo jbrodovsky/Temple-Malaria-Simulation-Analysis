77c9dc6544f65abd3b2aa735b7a648ef226e82b2
James Brodovsky <james.brodovsky@gmail.com>
2025-04-23 11:04:12 -0400

    added missing Moz calibration jobs

--- jobs/moz_1000_jobs.sh
+++ jobs/moz_1000_jobs.sh
@@ -2,6 +2,6 @@
 #PBS -l walltime=48:00:00
 #PBS -N moz_1000_jobs
 #PBS -q normal
-#PBS -l nodes=8:ppn=28
+#PBS -l nodes=3:ppn=15
 cd $PBS_O_WORKDIR
-torque-launch moz_1000_cmds.txt
+torque-launch missing_calibration_runs_1000_0.847.txt
--- jobs/moz_500_jobs.sh
+++ jobs/moz_500_jobs.sh
@@ -2,6 +2,6 @@
 #PBS -l walltime=48:00:00
 #PBS -N moz_500_jobs
 #PBS -q normal
-#PBS -l nodes=8:ppn=28
+#PBS -l nodes=3:ppn=15
 cd $PBS_O_WORKDIR
-torque-launch moz_500_cmds.txt
+torque-launch missing_calibration_runs_500_0.847.txt
