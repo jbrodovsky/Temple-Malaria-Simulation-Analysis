743b0e377ba8badbcea6ff0138d603d25a69a30b
James Brodovsky <james.brodovsky@gmail.com>
2025-05-14 13:41:20 -0400

    added notification and direct command for job

--- validation.sh
+++ validation.sh
@@ -2,6 +2,9 @@
 #PBS -l walltime=48:00:00
 #PBS -N validation
 #PBS -q normal
+#PBS -m bae
+#PBS -M jbrodovsky@temple.edu
 #PBS -l nodes=1:ppn=28
 cd $PBS_O_WORKDIR
-torque-launch moz_validation.txt
+
+./bin/MaSim -i conf/moz/calibration/moz_birth_rate_cal.yml -o output/moz/birth_validation/birth_calibration_ -r SQLiteDistrictReporter
