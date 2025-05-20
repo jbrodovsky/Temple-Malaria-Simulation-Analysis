2671e49412ccd4537419603fc4825d1b2bb81e28
James Brodovsky <james.brodovsky@gmail.com>
2025-05-20 15:47:40 -0400

    updated for nessun dorma cluster

--- validation.sh
+++ validation.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-#PBS -l walltime=48:00:00
+#!/bin/bash
+#PBS -l walltime=100:00:00
 #PBS -N validation
-#PBS -q normal
-#PBS -m bae
-#PBS -M jbrodovsky@temple.edu
-#PBS -l nodes=1:ppn=28
+#PBS -q nd
+#PBS -l select=1:ncpus=5:host=nd01
+#PBS -o submit_jobs.output
+#PBS -e submit_jobs.error
+
 cd $PBS_O_WORKDIR
 
-# module load gcc/12.2.0
-
-./bin/MaSim -i conf/moz/test.yml -o moz_test_ -r SQLiteDistrictReporter -j 98
+./bin/MaSim -i conf/moz/moz_test.yml -o moz_test_ -r SQLiteDistrictReporter -j 98
