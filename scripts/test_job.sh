a10dab8a38f4c9db1fa001aa8234907812e8f8cc
James Brodovsky <james.brodovsky@gmail.com>
2025-08-12 13:24:58 -0400

    modified job file

--- scripts/test_job.sh
+++ scripts/test_job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -l walltime=48:00:00
-#PBS -N moz_10_jobs
+#PBS -N test_job
 #PBS -q normal
-#PBS -l nodes=8:ppn=28
+#PBS -l nodes=1:ppn=28
 cd $PBS_O_WORKDIR
-torque-launch moz_10_cmds.txt
+torque-launch test_cmds.txt
